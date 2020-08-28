1dffbb64f31f43d4e16ac52f7b6d12e6a2f14f88
Guilheeeerme <guilherme_moura.10222@outlook.com>
2020-08-27 22:12:20 -0300

    add Saque e Saldo

--- Módulo4-Persistência de dados, versionamento e implantação Heroku/TrabalhoPratico/routes/accountRouter.js
+++ Módulo4-Persistência de dados, versionamento e implantação Heroku/TrabalhoPratico/routes/accountRouter.js
@@ -22,4 +22,44 @@ app.put("/deposito", async (req, res) => {
   }
 });
 
+app.put("/saque", async (req, res) => {
+  const { agencia, conta, balance } = req.body;
+  if (!agencia || !conta || !balance) {
+    return res.status(400).send("Parâmetros inválidos");
+  }
+
+  try {
+    const data = await accountModel.findOne({ agencia, conta });
+    if (!data) {
+      res.status(404).send("Conta inexistente");
+    }
+    const newBalance = (data.balance -= balance - 1);
+    if (newBalance < 0) {
+      return res.status(400).send("Saldo insuficiente");
+    }
+    data.balance = newBalance;
+    await data.save();
+    res.send(data);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.get("/saldo", async (req, res) => {
+  const { agencia, conta } = req.body;
+  if (!agencia || !conta) {
+    return res.status(400).send("Parâmetros inválidos");
+  }
+
+  try {
+    const data = await accountModel.findOne({ agencia, conta });
+    if (!data) {
+      res.status(404).send("Conta inexistente");
+    }
+    res.send(`Balance: R$ ${data.balance}`);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
 export { app as accountRouter };
