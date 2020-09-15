5c46e37cbb169234356c03d47e1d018343c7ca08
Guilheeeerme <guilherme_moura.10222@outlook.com>
2020-09-15 19:57:45 -0300

    update função createJsonFile

--- Módulo2-Desenvolvimento de API's/TrabalhoPratico/index.js
+++ Módulo2-Desenvolvimento de API's/TrabalhoPratico/index.js
@@ -1,51 +1,46 @@
-const fs = require("fs");
-const path = require("path");
-
-const cities = require("./data/Cidades.json");
-const states = require("./data/Estados.json");
-
-const folder = path.join(__dirname, "/output/");
-
-const info = [];
+const fs = require("fs").promises;
 
 /* Criar uma função que irá criar um arquivo JSON para cada estado representado no arquivo Estado.json,
 e o seu conteúdo será um array das cidades pertencentes a aquele estado, de acordo com o arquivo Cidades.json.
 O nome do arquivo deve ser o UF do estado, por exemplo: MG.json. */
-function getInfos() {
-  states.forEach((state) => {
-    info.push(cities.filter((city) => city.Estado === state.ID));
-  });
-  return info;
-}
-getInfos();
 
-// prettier-ignore
 async function createJsonFile() {
-  for (let i = 0; i < info.length; i++)
-    await fs.writeFile(`${folder}${states[i].Sigla}.json`, JSON.stringify(info[i], null, 2),
-    (err) => {
-      if (err) throw err;
-      console.log(err);
-    });
+  let data = await fs.readFile("./data/Estados.json");
+  const states = JSON.parse(data);
+
+  data = await fs.readFile("./data/Cidades.json");
+  const cities = JSON.parse(data);
+
+  for (state of states) {
+    const stateCities = cities.filter((city) => city.Estado === state.ID);
+    await fs.writeFile(
+      `./output/${state.Sigla}.json`,
+      JSON.stringify(stateCities, null, 2),
+      (err) => {
+        if (err) throw err;
+        console.log(err);
+      }
+    );
+  }
 }
 createJsonFile();
 
 /* Criar uma função que recebe como parâmetro o UF do estado, realize a leitura do
 arquivo JSON correspondente e retorne a quantidade de cidades daquele estado. */
 // prettier-ignore
-async function numberOfCities(uf) {
-  uf = uf.toLowerCase();
-  await fs.readFile(`${folder}${uf}.json`, (err, data) => {
-    if (err) throw err;
+// async function numberOfCities(uf) {
+//   uf = uf.toLowerCase();
+//   await fs.readFile(`${folder}${uf}.json`, (err, data) => {
+//     if (err) throw err;
 
-    let cities = JSON.parse(data);
-    let mappedCities = cities.map((city) => {
-      return {
-        Cidade: city.Nome,
-      };
-    });
-    console.log(`O estado ${uf.toUpperCase()} possui ${mappedCities.length} cidades`);
-    console.log(mappedCities);
-  });
-}
-numberOfCities("ac");
+//     let cities = JSON.parse(data);
+//     let mappedCities = cities.map((city) => {
+//       return {
+//         Cidade: city.Nome,
+//       };
+//     });
+//     console.log(`O estado ${uf.toUpperCase()} possui ${mappedCities.length} cidades`);
+//     console.log(mappedCities);
+//   });
+// }
+// numberOfCities("ac");
