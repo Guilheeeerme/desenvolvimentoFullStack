f343c8334a2940905ed1434a8b082315f15a0313
Guilherme Moura <61475431+Guilheeeerme@users.noreply.github.com>
2020-09-14 22:14:49 -0300

    Add conexão com heroku

--- Módulo4-Persistência de dados, versionamento e implantação Heroku/Desafio/grades-api/app.js
+++ Módulo4-Persistência de dados, versionamento e implantação Heroku/Desafio/grades-api/app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:8080',
+    origin: 'https://api-front-desafio-igti.herokuapp.com',
   })
 );
 
