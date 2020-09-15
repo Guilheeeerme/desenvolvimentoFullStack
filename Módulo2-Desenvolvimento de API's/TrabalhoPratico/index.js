cdc6780984f1517788759c5ac383b397ef72e90a
Guilheeeerme <guilherme_moura.10222@outlook.com>
2020-09-15 20:12:25 -0300

    add função getCitiesCount

--- Módulo2-Desenvolvimento de API's/TrabalhoPratico/index.js
+++ Módulo2-Desenvolvimento de API's/TrabalhoPratico/index.js
@@ -1,9 +1,14 @@
-const fs = require("fs").promises;
+const fs = require("fs").promises; // Evitar precisar fazer then/catch
+
+init();
+async function init() {
+  await createJsonFile();
+  console.log(await getCitiesCount("SP"));
+}
 
 /* Criar uma função que irá criar um arquivo JSON para cada estado representado no arquivo Estado.json,
 e o seu conteúdo será um array das cidades pertencentes a aquele estado, de acordo com o arquivo Cidades.json.
 O nome do arquivo deve ser o UF do estado, por exemplo: MG.json. */
-
 async function createJsonFile() {
   let data = await fs.readFile("./data/Estados.json");
   const states = JSON.parse(data);
@@ -23,24 +28,11 @@ async function createJsonFile() {
     );
   }
 }
-createJsonFile();
 
 /* Criar uma função que recebe como parâmetro o UF do estado, realize a leitura do
 arquivo JSON correspondente e retorne a quantidade de cidades daquele estado. */
-// prettier-ignore
-// async function numberOfCities(uf) {
-//   uf = uf.toLowerCase();
-//   await fs.readFile(`${folder}${uf}.json`, (err, data) => {
-//     if (err) throw err;
-
-//     let cities = JSON.parse(data);
-//     let mappedCities = cities.map((city) => {
-//       return {
-//         Cidade: city.Nome,
-//       };
-//     });
-//     console.log(`O estado ${uf.toUpperCase()} possui ${mappedCities.length} cidades`);
-//     console.log(mappedCities);
-//   });
-// }
-// numberOfCities("ac");
+async function getCitiesCount(uf) {
+  const data = await fs.readFile(`./output/${uf}.json`);
+  const cities = JSON.parse(data);
+  return `O estado ${uf} tem ${cities.length} cidades.`;
+}
