b54e5ecf37cea3ffbd9528f93cd35b21ad4d48ca
Guilheeeerme <guilherme_moura.10222@outlook.com>
2020-09-16 18:17:42 -0300

    add função getBiggerNameCities

--- Módulo2-Desenvolvimento de API's/TrabalhoPratico/index.js
+++ Módulo2-Desenvolvimento de API's/TrabalhoPratico/index.js
@@ -5,7 +5,9 @@ const fs = require("fs").promises; // Evitar precisar fazer then/catch
 init();
 async function init() {
   await createJsonFile();
-  await getStatesWithMoreCities();
+  await getStatesWithMoreOrLessCities(true); // para pegar os estados que tem mais cidades
+  await getStatesWithMoreOrLessCities(false); // para pegar os estados que tem menos cidades
+  await getBiggerNameCities();
 }
 
 /* Criar uma função que irá criar um arquivo JSON para cada estado representado no arquivo Estado.json,
@@ -40,11 +42,10 @@ async function getCitiesCount(uf) {
 }
 
 // Criar um método que imprima no console um array com o UF dos cinco estados
-// que mais possuem cidades, seguidos da quantidade, em ordem decrescente. Você
+// que mais possuem cidades e dos cinco que menos possuem cidades, seguidos da quantidade, em ordem decrescente. Você
 // pode usar a função criada no tópico 2. Exemplo de impressão: [“UF - 93”, “UF - 82”,
 // “UF - 74”, “UF - 72”, “UF - 65”]
-
-async function getStatesWithMoreCities() {
+async function getStatesWithMoreOrLessCities(more) {
   const states = JSON.parse(await fs.readFile("./data/Estados.json"));
   const list = [];
 
@@ -60,7 +61,40 @@ async function getStatesWithMoreCities() {
   });
 
   const result = [];
-  list.slice(0, 5).forEach((item) => result.push(item.uf + " - " + item.count));
 
+  if (more) {
+    list
+      .slice(0, 5)
+      .forEach((item) => result.push(`${item.uf} - ${item.count}`));
+  } else {
+    list.slice(-5).forEach((item) => result.push(item.uf + " - " + item.count));
+  }
+}
+
+// Criar um método que imprima no console um array com a cidade de maior nome de
+// cada estado, seguida de seu UF. Por exemplo: [“Nome da Cidade – UF”, “Nome da
+// Cidade – UF”, ...].
+async function getBiggerNameCities() {
+  const states = JSON.parse(await fs.readFile("./data/Estados.json"));
+  const result = [];
+  for (state of states) {
+    const city = await getBiggerName(state.Sigla);
+    result.push(`${city.Nome} - ${state.Sigla}`);
+  }
   console.log(result);
+}
+
+async function getBiggerName(uf) {
+  const cities = JSON.parse(await fs.readFile(`./output/${uf}.json`));
+
+  let result;
+
+  // prettier-ignore
+  cities.forEach((city) => {
+    if (!result) result = city;
+    else if (city.Nome.length > result.Nome.length) result = city;
+    else if (city.Nome.length === result.Nome.length && city.Nome.toLowerCase() < result.Nome.toLowerCase()) result = city;
+  });
+
+  return result;
 }
